8bfd95aabdbfff6c31060f9e32143a31072a9fe7
conct <daniel.l@conct.de>
2017-12-01 07:25:57 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-const io = require('socket.io-client'
-)
-const socket = io.connect('http://conct.de:62751', { transports:['websocket'] })
+const socket = require('socket.io-client')('http://conct.de:62751', { transports:['websocket'] })
 
 module.exports = socket;
